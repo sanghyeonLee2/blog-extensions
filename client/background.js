dff83cdd047eea65dd91bfe0b1e674900bf3d70c
sanghyeonLee2 <hyeon012366@gmail.com>
2024-06-09 18:39:28 +0900

    feat : 크롬 익스텐션 개발 완료

--- client/background.js
+++ client/background.js
@@ -1,25 +1,67 @@
+// 확장 프로그램이 설치될 때 실행되는 코드
 chrome.runtime.onInstalled.addListener(() => {
-    console.log('Extension installed');
-    // 컨텍스트 메뉴 아이템 생성
-    chrome.contextMenus.create({
-        id: 'fetchPageTitle',
-        title: 'Fetch Page Title',
-        contexts: ['link'],
-    });
+  console.log("Extension installed");
+  setupContextMenu();
 });
 
+// 컨텍스트 메뉴 설정 함수
+function setupContextMenu() {
+  chrome.contextMenus.removeAll(() => {
+    chrome.contextMenus.create({
+      id: "fetchPageTitle",
+      title: "홍보성 체크",
+      contexts: ["link"],
+    });
+  });
+}
+
+// 컨텍스트 메뉴 클릭 이벤트 리스너
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-    console.log(info)
-    if (info.menuItemId === 'fetchPageTitle') {
-        fetch(info.linkUrl)
-            .then(response => response.text())
-            .then(html => {
-                console.log(html)
-                const matches = html.match(/<title>(.*?)<\/title>/);
-                const title = matches && matches[1] ? matches[1] : "Title not found";
-                console.log(title); // 콘솔에 타이틀을 출력합니다.
-                // 여기에서 더 많은 동작을 추가할 수 있습니다. 예를 들어, 결과를 탭에 메시지로 보낼 수 있습니다.
-            })
-            .catch(error => console.error('Error fetching the page:', error));
-    }
+  if (info.menuItemId === "fetchPageTitle") {
+    executeFetchPageTitleScript(tab.id, info.linkUrl);
+  }
 });
+
+// 스크립트 실행 및 메시지 보내는 함수
+async function executeFetchPageTitleScript(tabId, linkUrl) {
+  try {
+    await executeScriptAsync({
+      target: { tabId: tabId },
+      files: ["./scripts/content.js"],
+    });
+
+    const response = await sendMessageAsync(tabId, {
+      action: "fetchPageTitle",
+      url: linkUrl,
+    });
+    console.log("Page title:", response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// chrome.scripting.executeScript를 Promise로 래핑한 함수
+function executeScriptAsync(details) {
+  return new Promise((resolve, reject) => {
+    chrome.scripting.executeScript(details, () => {
+      if (chrome.runtime.lastError) {
+        reject(new Error(chrome.runtime.lastError));
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+// chrome.tabs.sendMessage를 Promise로 래핑한 함수
+function sendMessageAsync(tabId, message) {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.sendMessage(tabId, message, (response) => {
+      if (chrome.runtime.lastError) {
+        reject(new Error(chrome.runtime.lastError));
+      } else {
+        resolve(response);
+      }
+    });
+  });
+}
--- client/popup/popup.js
+++ client/popup/popup.js
@@ -1,1 +1,45 @@
-console.log("This is a popup!")
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("fetchContent");
+  button.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tabs[0].id },
+          files: ["./scripts/content.js"],
+        },
+        () => {
+          // content.js 파일이 실행된 후, 현재 탭에 메시지를 보냅니다.
+          chrome.tabs.sendMessage(
+            tabs[0].id,
+            { action: "fetchBlogContent" },
+            (response) => {
+              console.log("Received response:", response); // 디버깅용 로그 추가
+              if (response && response.predictions && response.probabilities) {
+                const predictions = response.predictions;
+                const probabilities = response.probabilities;
+
+                const resultElement = document.getElementById("result"); // 결과를 표시할 요소의 ID
+                const rateResultElement = document.getElementById("rateResult"); // 결과를 표시할 요소의 ID
+                if (predictions[0] === 0) {
+                  resultElement.textContent = "홍보성이 아닙니다";
+                  rateResultElement.textContent =
+                    "리뷰가 홍보성일 확률 : " +
+                    probabilities[0][0].toFixed(1) +
+                    "%";
+                } else {
+                  resultElement.textContent = "홍보성입니다";
+                  rateResultElement.textContent =
+                    "리뷰가 홍보성일 확률 : " +
+                    (100 - probabilities[0][0].toFixed(1)) +
+                    "%";
+                }
+              } else {
+                console.error("Invalid response structure:", response);
+              }
+            }
+          );
+        }
+      );
+    });
+  });
+});
